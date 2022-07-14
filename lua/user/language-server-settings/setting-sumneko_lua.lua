ab7356cc483301476e0b22cb5cd061e11e82dce9
mufHaQ <masbrowmess33@gmail.com>
2022-07-14 16:35:42 +0700

    update

--- lua/user/language-server-settings/setting-sumneko_lua.lua
+++ lua/user/language-server-settings/setting-sumneko_lua.lua
@@ -3,24 +3,24 @@ return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
 	root_dir = function(fname)
-		return util.find_git_ancestor(fname) or util.path.dirname(fname)
+		return util.root_pattern(".luarc.json", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", ".git")
+			or util.path.dirname(fname)
 	end,
 	single_file_support = true,
 	settings = {
 		Lua = {
 			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
 			},
 			diagnostics = {
-				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
 			},
 			workspace = {
-				-- Make the server aware of Neovim runtime files
 				library = vim.api.nvim_get_runtime_file("", true),
+				--[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+				--[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				--[vim.fn.stdpath("config") .. "/lua"] = true,
 			},
-			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
 				enable = false,
 			},
