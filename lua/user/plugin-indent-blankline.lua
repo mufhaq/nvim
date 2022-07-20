e55fc59429d337002015ebc33f52283da9dbfe22
mufHaQ <masbrowmess33@gmail.com>
2022-07-20 19:27:45 +0700

    update indent blankline, show end of line

--- lua/user/plugin-indent-blankline.lua
+++ lua/user/plugin-indent-blankline.lua
@@ -1,6 +1,10 @@
+vim.opt.list = true
+vim.opt.listchars:append("eol:↴")
+
 require("indent_blankline").setup({
 	show_current_context = false,
 	show_current_context_start = false,
 	show_first_indent_level = true,
 	show_trailing_blankline_indent = false,
+	show_end_of_line = true,
 })
