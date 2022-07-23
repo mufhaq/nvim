99a890c9ef6bca458e3e22ad20322c68e3a6cbce
mufHaQ <masbrowmess33@gmail.com>
2022-07-23 18:36:31 +0700

    update

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ require("user.plugin-impatient")
 require("user.plugin-colorizer")
 require("user.plugin-sniprun")
 require("user.plugin-nvim-tree")
-require("colorscheme").vscode()
+require("colorscheme").onedark()
 
 -- keep this line at the very bottom
 require("functions").init()
