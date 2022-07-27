5c0cffbe77a5910b73afc03f63fbbc3bdf9532f0
mufHaQ <masbrowmess33@gmail.com>
2022-07-27 19:42:21 +0700

    add keymap to open url

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,6 +10,16 @@ local opts = { noremap = true, silent = true }
 --  term_mode           = 't'
 --  command_mode        = 'c'
 
+-- Neovim
+-- Open Link
+if vim.fn.has("mac") == 1 then
+	keymap("n", "gx", '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>', opts)
+elseif vim.fn.has("unix") == 1 then
+	keymap("n", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', opts)
+else
+	keymap("n", "gx", '<Cmd>lua print("Error: gx is not supported on this OS!")<CR>', opts)
+end
+
 -- Telescope
 keymap("n", "<C-p>", require("telescope.builtin").find_files, opts)
 keymap("n", "<leader>l", require("telescope.builtin").live_grep, opts)
