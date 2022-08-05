3d47ef6866eff9550e4ca56a829aae9b6cce720c
mufHaQ <masbrowmess33@gmail.com>
2022-08-06 01:23:03 +0700

    disable format on save for null ls

--- lua/user/plugin-null-ls.lua
+++ lua/user/plugin-null-ls.lua
@@ -22,17 +22,18 @@ null_ls.setup({
 			extra_args = { "-style={IndentWidth: 4}" },
 		}),
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.formatting_sync()
-				end,
-			})
-		end
-	end,
+	--on_attach = function(client, bufnr)
+	--	-- Format On Save
+	--	if client.supports_method("textDocument/formatting") then
+	--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	--		vim.api.nvim_create_autocmd("BufWritePre", {
+	--			group = augroup,
+	--			buffer = bufnr,
+	--			callback = function()
+	--				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+	--				vim.lsp.buf.formatting_sync()
+	--			end,
+	--		})
+	--	end
+	--end,
 })
