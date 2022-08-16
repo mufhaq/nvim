5e7e076f956c74d38892a6744b81bfa042b8d40a
mufHaQ <masbrowmess33@gmail.com>
2022-08-17 03:56:29 +0700

    update

--- lua/user/language-server-settings/setting-jdtls.lua
+++ lua/user/language-server-settings/setting-jdtls.lua
@@ -9,7 +9,7 @@ return {
 	filetypes = { "java" },
 	init_options = {
 		jvm_args = {},
-		workspace = "/home/runner/.cache/jdtls/workspace",
+		workspace = "/home/haq/.cache/jdtls/workspace",
 	},
 	single_file_support = true,
 }
