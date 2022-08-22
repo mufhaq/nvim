444b5da970475dc15d84ac9a5ae49e868794c89e
mufHaQ <masbrowmess33@gmail.com>
2022-08-22 07:30:18 +0700

    update

--- lua/user/setting-indent-blankline.lua
+++ lua/user/setting-indent-blankline.lua
@@ -3,7 +3,7 @@ vim.opt.list = true
 --vim.opt.listchars:append(space = "⋅")
 
 require("indent_blankline").setup({
-	show_current_context = false,
+	show_current_context = true,
 	show_current_context_start = false,
 	show_first_indent_level = true,
 	show_trailing_blankline_indent = false,
