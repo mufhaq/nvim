b9b0cbbb95b31159facdea82e23e6088d9e789a7
mufHaQ <masbrowmess33@gmail.com>
2022-08-24 07:28:13 +0700

    update

--- lua/user/setting-bufferline.lua
+++ lua/user/setting-bufferline.lua
@@ -6,12 +6,19 @@ require("bufferline").setup({
 		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
 		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-		indicator = "▎", -- Default: ▎
+		indicator = {
+			icon = "▎", -- this should be omitted if indicator style is not 'icon'
+			style = "icon",
+		},
 		buffer_close_icon = "",
 		modified_icon = "●",
 		close_icon = "",
 		left_trunc_marker = "",
 		right_trunc_marker = "",
+		--- name_formatter can be used to change the buffer's label in the bufferline.
+		--- Please note some names can/will break the
+		--- bufferline so use this at your discretion knowing that it has
+		--- some limitations that will *NOT* be fixed.
 		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
 			-- remove extension from markdown files for example
 			if buf.name:match("%.md") then
@@ -22,9 +29,9 @@ require("bufferline").setup({
 		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
 		tab_size = 18,
 		diagnostics = "nvim_lsp",
-		diagnostics_update_in_insert = true,
+		diagnostics_update_in_insert = false,
 		-- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
-		diagnostics_indicator = function(count, _, _, _)
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
 			return "(" .. count .. ")"
 		end,
 		-- NOTE: this will be called a lot so don't do any heavy processing here
@@ -47,14 +54,23 @@ require("bufferline").setup({
 				return true
 			end
 		end,
-		offsets = { { filetype = "NvimTree", text = "" } },
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				text_align = "center",
+				separator = true,
+			},
+		},
 		color_icons = true, -- whether or not to add the filetype icon highlights
 		show_buffer_icons = true, -- disable filetype icons for buffers
 		show_buffer_close_icons = true,
 		show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
-		show_close_icon = false,
-		show_tab_indicators = false,
+		show_close_icon = true,
+		show_tab_indicators = true,
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+		-- can also be a table containing 2 custom separators
+		-- [focused and unfocused]. eg: { '|', '|' }
 		separator_style = { "", "" },
 		enforce_regular_tabs = true,
 		always_show_bufferline = true,
