ae20f12d670d9a060866ea0246ac02fdf09d86ee
mufHaQ <masbrowmess33@gmail.com>
2022-08-25 08:44:03 +0700

    remove redundant wrap

--- lua/options.lua
+++ lua/options.lua
@@ -33,7 +33,6 @@ vim.opt.ignorecase = true
 vim.opt.so = 5
 vim.opt.termguicolors = true
 vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.wrap = false
 vim.opt.colorcolumn = "80"
 vim.opt.signcolumn = "yes"
 vim.opt.wrap = true
