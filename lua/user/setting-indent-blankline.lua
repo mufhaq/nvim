0813db3af7f217f39b5b132933fa28dfda7f8af5
mufhaq <masbrowmess33@gmail.com>
2022-09-04 14:10:09 +0700

    indent-blankline change value of show_trailing_blankline_indent

--- lua/user/setting-indent-blankline.lua
+++ lua/user/setting-indent-blankline.lua
@@ -8,6 +8,6 @@ require("indent_blankline").setup({
 	show_current_context = true,
 	show_current_context_start = false,
 	show_first_indent_level = true,
-	show_trailing_blankline_indent = true,
+	show_trailing_blankline_indent = false,
 	show_end_of_line = true,
 })
