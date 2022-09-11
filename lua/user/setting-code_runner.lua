c5806d6e6f240e1311a0a74dfdf0ee139387518d
mufhaq <masbrowmess33@gmail.com>
2022-09-11 15:17:31 +0700

    update

--- lua/user/setting-code_runner.lua
+++ lua/user/setting-code_runner.lua
@@ -4,7 +4,7 @@ require("code_runner").setup({
 	startinsert = false,
 	term = {
 		position = "vert",
-		size = 55,
+		size = 65,
 	},
 	filetype_path = vim.fn.expand("~/.config/nvim/code_runner.json"),
 	project_path = vim.fn.expand("~/.config/nvim/project_manager.json"),
