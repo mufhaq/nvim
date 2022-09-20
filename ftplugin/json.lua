d24431fc3415c8957a32c5a4704aa967aec9946a
mufhaq <masbrowmess33@gmail.com>
2022-09-21 03:54:10 +0700

    [ftplugin-json] add jsonc config for tsconfig.json and jsconfig.json

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,3 +1,8 @@
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+
+local fname = vim.fn.expand("%:t")
+if fname == "jsconfig.json" or fname == "tsconfig.json" then
+	vim.bo.filetype = "jsonc"
+end
