0d4266bd7991cbad56d77074fc4e5b2309190fed
mufhaq <masbrowmess33@gmail.com>
2022-11-24 05:07:04 +0700

    update tsserver config

--- lua/user/language-server-settings/server-tsserver.lua
+++ lua/user/language-server-settings/server-tsserver.lua
@@ -17,8 +17,7 @@ return {
 		},
 	},
 	root_dir = function(fname)
-		return util.root_pattern("tsconfig.json")(fname)
-			or util.root_pattern("package.json", "jsconfig.json", ".git")(fname)
-			or util.path.dirname(fname)
+		return util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")(fname)
+		-- or util.path.dirname(fname)
 	end,
 }
