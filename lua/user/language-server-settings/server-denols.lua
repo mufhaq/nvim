6bcdf555871793f94e90fffe2d94c994fd11dfad
mufhaq <masbrowmess33@gmail.com>
2022-11-24 04:47:42 +0700

    denols enable lint

--- lua/user/language-server-settings/server-denols.lua
+++ lua/user/language-server-settings/server-denols.lua
@@ -5,6 +5,7 @@ return {
 	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 	init_options = {
 		enable = true,
+		lint = true,
 		unstable = false,
 	},
 	root = util.root_pattern("deno.json", "deno.jsonc", ".git"),
