33e91fd71becd9baf8d505dd1f174cb020cfca9a
mufhaq <masbrowmess33@gmail.com>
2023-04-10 16:46:24 +0700

    [keymaps] add prefix to keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -28,11 +28,11 @@ else
 end
 
 -- Telescope
-keymap("n", "<C-p>", require("telescope.builtin").find_files, opts)
-keymap("n", "<leader>l", require("telescope.builtin").live_grep, opts)
-keymap("n", "<leader>b", require("telescope.builtin").buffers, opts)
-keymap("n", "<leader>s", require("telescope.builtin").grep_string, opts)
-keymap("n", "<leader>f", require("telescope").extensions.file_browser.file_browser, opts)
+keymap("n", "<C-p>p", require("telescope.builtin").find_files, opts)
+keymap("n", "<C-p>l", require("telescope.builtin").live_grep, opts)
+keymap("n", "<C-p>b", require("telescope.builtin").buffers, opts)
+keymap("n", "<C-p>s", require("telescope.builtin").grep_string, opts)
+keymap("n", "<C-p>f", require("telescope").extensions.file_browser.file_browser, opts)
 
 -- Sniprun
 keymap("n", "<F5>", functions.sniprun, opts)
