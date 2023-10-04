d7ccabb05cdd064786c89c4622dc5713f1f3da92
mufhaq <masbrowmess33@gmail.com>
2023-10-05 04:53:32 +0700

    [plugin] migrate to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,62 +1,74 @@
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+require("lazy").setup({
 	-- Colorschemes
-	use("morhetz/gruvbox")
-	use("Mofiqul/vscode.nvim")
-
-	-- LSP
-	use("neovim/nvim-lspconfig")
-	use({
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-	})
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	})
-	use({
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-	})
-	use("mfussenegger/nvim-jdtls")
-	use("simrat39/rust-tools.nvim")
-	use("folke/trouble.nvim")
-	use("ray-x/lsp_signature.nvim")
-
-	-- CMP
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("L3MON4D3/LuaSnip")
-	use("hrsh7th/cmp-nvim-lua")
-	use("saadparwaiz1/cmp_luasnip")
-
-	-- Latex
-	use("lervag/vimtex")
-
-	-- Others
-	use("nvim-tree/nvim-web-devicons")
-	use("nvim-lua/plenary.nvim")
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-file-browser.nvim")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use({ "nvim-lualine/lualine.nvim" })
-	use("lewis6991/gitsigns.nvim")
-	use("lukas-reineke/indent-blankline.nvim")
-	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install" })
-	use("lewis6991/impatient.nvim")
-	use("norcalli/nvim-colorizer.lua")
-	use({ "michaelb/sniprun", run = "bash ./install.sh" })
-	use({ "kyazdani42/nvim-tree.lua", tag = "nightly" })
-	use({ "projekt0n/github-nvim-theme" })
-	use({ "akinsho/bufferline.nvim", tag = "v2.*" })
-	use("numToStr/Comment.nvim")
-	use("windwp/nvim-autopairs")
-	use("CRAG666/code_runner.nvim")
-	use("b0o/schemastore.nvim")
-end)
+	"morhetz/gruvbox",
+	"Mofiqul/vscode.nvim",
+ 
+ 	-- LSP
+ 	"neovim/nvim-lspconfig",
+ 	{
+ 		"williamboman/mason.nvim",
+ 		"williamboman/mason-lspconfig.nvim",
+ 	},
+ 	{
+ 		"nvim-treesitter/nvim-treesitter",
+ 		run = function()
+ 			require("nvim-treesitter.install").update({ with_sync = true })
+ 		end,
+ 	},
+ 	{
+ 		"j-hui/fidget.nvim",
+ 		tag = "legacy",
+ 	},
+ 	"mfussenegger/nvim-jdtls",
+ 	"simrat39/rust-tools.nvim",
+ 	"folke/trouble.nvim",
+ 	"ray-x/lsp_signature.nvim",
+ 
+ 	-- CMP
+ 	"hrsh7th/cmp-nvim-lsp",
+ 	"hrsh7th/cmp-buffer",
+ 	"hrsh7th/cmp-path",
+ 	"hrsh7th/cmp-cmdline",
+ 	"hrsh7th/nvim-cmp",
+ 	"L3MON4D3/LuaSnip",
+ 	"hrsh7th/cmp-nvim-lua",
+ 	"saadparwaiz1/cmp_luasnip",
+ 
+ 	-- Latex
+ 	"lervag/vimtex",
+ 
+ 	-- Others
+ 	"nvim-tree/nvim-web-devicons",
+ 	"nvim-lua/plenary.nvim",
+ 	"nvim-telescope/telescope.nvim",
+ 	"nvim-telescope/telescope-file-browser.nvim",
+ 	"jose-elias-alvarez/null-ls.nvim",
+ 	{ "nvim-lualine/lualine.nvim" },
+ 	"lewis6991/gitsigns.nvim",
+ 	"lukas-reineke/indent-blankline.nvim",
+ 	{ "iamcco/markdown-preview.nvim", run = "cd app && yarn install" },
+ 	"lewis6991/impatient.nvim",
+ 	"norcalli/nvim-colorizer.lua",
+ 	{ "michaelb/sniprun", run = "bash ./install.sh" },
+ 	{ "kyazdani42/nvim-tree.lua", tag = "nightly" },
+ 	{ "projekt0n/github-nvim-theme" },
+ 	{ "akinsho/bufferline.nvim", tag = "v2.*" },
+ 	"numToStr/Comment.nvim",
+ 	"windwp/nvim-autopairs",
+ 	"CRAG666/code_runner.nvim",
+ 	"b0o/schemastore.nvim",
+})
