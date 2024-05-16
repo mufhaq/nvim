4e15c52d61d5a4fa6d0d7526bc6173c6824e01dd
mufhaq <masbrowmess33@gmail.com>
2024-05-16 19:10:42 +0700

    [globals] add config for vimtex latexmk

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,1 +1,9 @@
 vim.g.USER = os.getenv("USER")
+
+vim.g.vimtex_view_general_viewer = "okular"
+vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_compiler_latexmk = {
+	["aux_dir"] = "out/aux",
+	["out_dir"] = "out/build",
+}
