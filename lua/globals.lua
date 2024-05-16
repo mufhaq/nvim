76fbfe1600d6711ac1f7007772e44c8c0f9237c7
mufhaq <masbrowmess33@gmail.com>
2024-05-16 19:14:49 +0700

    [globals] update vimtex compile and aux dir

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,9 +1,11 @@
 vim.g.USER = os.getenv("USER")
 
+local latex_cwd = vim.fn.getcwd()
+
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_compiler_latexmk = {
-	["aux_dir"] = "out/aux",
-	["out_dir"] = "out/build",
+	["aux_dir"] = latex_cwd .. "/.out/aux",
+	["out_dir"] = latex_cwd .. "/.out/build",
 }
